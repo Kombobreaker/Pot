e0284a2317997ae2ff5948492a0377ede2505815
Kombobreaker <40173545+Kombobreaker@users.noreply.github.com>
2018-06-12 20:51:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,62 +1,31 @@
-var APP_PREFIX = 'Pot'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'v_01'              // Version of the off-line cache (change this value everytime you want to update cache)
-var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [                            // Add URL you want to cache in this list.
-  '/Pot/',                     // If you have separate JS/CSS files,
-  '/Pot/index.html',           // add path to those files here
-  '/Pot/manifest.json',
-  'https://unpkg.com/onsenui/css/onsenui.min.css',
-  'https://unpkg.com/onsenui/css/onsen-css-components.min.css',
-  'https://unpkg.com/onsenui/js/onsenui.min.js'
-]
+self.addEventListener('install', function(e) {
+  console.log('install');
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
+  // waitUntil tells the browser that the install event is not finished until we have
+  // cached all of our files
+  e.waitUntil(
+    // Here we call our cache "myonsenuipwa", but you can name it anything unique
+    caches.open('potpwa').then(cache => {
+      // If the request for any of these resources fails, _none_ of the resources will be
+      // added to the cache.
+      return cache.addAll([
+        '/Pot/',
+        '/Pot/index.html',
+        '/Pot/manifest.json',
+        'https://unpkg.com/onsenui/css/onsenui.min.css',
+        'https://unpkg.com/onsenui/css/onsen-css-components.min.css',
+        'https://unpkg.com/onsenui/js/onsenui.min.js'
+      ]);
+    })
+  );
+});
+
+// 2. Intercept requests and return the cached version instead
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
-    })
-  )
-})
-
-// Cache resources
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(URLS)
-    })
-  )
-})
-
-// Delete outdated caches
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create white list
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
-      // add current cache name to white list
-      cacheWhitelist.push(CACHE_NAME)
-
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
-        }
-      }))
-    })
-  )
-})
+    // check if this file exists in the cache
+    caches.match(e.request)
+      // Return the cached file, or else try to get it from the server
+      .then(response => response || fetch(e.request))
+  );
+});
